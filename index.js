19584fa7262fa6c5463b2b12834eb703860da6fd
Prantadas7 <pranta_das@coredevs.ltd>
2022-11-01 18:34:43 +0600

    unzip done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { exec } = require('child_process');
 const os = require('os');
 const axios = require('axios');
 const fs = require('fs');
+const zlib = require('zlib');
+const jszip = require('jszip');
 
 const getChromeVersion = new Promise((resolve, reject) => {
     const currentPlatform = os.platform();
@@ -19,7 +21,6 @@ const getChromeVersion = new Promise((resolve, reject) => {
                 reject(err);
             }
             resolve(output.split(' ')[2].split('.')[0]);
-            // return console.log(version)
         });
     }
     else {
@@ -39,18 +40,18 @@ const downLoadDriver = async () => {
         // for detecting current version of chrome
         const currentVersion = await getChromeVersion;
         const { data } = await axios.get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE');
-        // if (data.split('.')[0] !== currentVersion) {
-        //     throw 'Please install the latest version of chrome'
-        // }
         if (currentVersion === '105' && currentPlatform.includes('linux')) {
-            const { data } = await axios.get('https://chromedriver.storage.googleapis.com/105.0.5195.52/chromedriver_linux64.zip');
+            const { data } = await axios.get('https://chromedriver.storage.googleapis.com/105.0.5195.52/chromedriver_linux64.zip', { responseType: 'arraybuffer' });
             fs.writeFileSync('chromedriver.zip', data, (err) => {
                 if (err) {
                     console.log(err);
                 }
-                console.log('File downloaded succesfully')
             });
-            // await fs.createReadStream('/chromedriver.zip').pipe(unzip.Extract({ path: '/chrome.exe' }));
+
+            const inStream = fs.createReadStream('chromedriver.zip', 'binary');
+            const outStream = fs.createWriteStream('chormedriver', 'binary');
+            const unZip = zlib.createDeflate();
+            inStream.pipe(unZip).pipe(outStream);
 
         }
 
