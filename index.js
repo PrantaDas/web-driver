fc890fb9e6ff5728e57ad0f57a4fc5559a9b1950
Prantadas7 <pranta_das@coredevs.ltd>
2022-11-02 13:45:03 +0600

    no progress

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const os = require('os');
 const axios = require('axios');
 const fs = require('fs');
 const zlib = require('zlib');
-const jszip = require('jszip');
+
 
 const getChromeVersion = new Promise((resolve, reject) => {
     const currentPlatform = os.platform();
@@ -41,16 +41,17 @@ const downLoadDriver = async () => {
         const currentVersion = await getChromeVersion;
         const { data } = await axios.get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE');
         if (currentVersion === '105' && currentPlatform.includes('linux')) {
-            const { data } = await axios.get('https://chromedriver.storage.googleapis.com/105.0.5195.52/chromedriver_linux64.zip', { responseType: 'arraybuffer' });
+            const url = 'https://chromedriver.storage.googleapis.com/105.0.5195.52/chromedriver_linux64.zip';
+            const { data } = await axios.get(url, { responseType: 'arraybuffer' });
             fs.writeFileSync('chromedriver.zip', data, (err) => {
                 if (err) {
                     console.log(err);
                 }
             });
 
-            const inStream = fs.createReadStream('chromedriver.zip', 'binary');
-            const outStream = fs.createWriteStream('chormedriver', 'binary');
-            const unZip = zlib.createDeflate();
+            const inStream = fs.createReadStream('chromedriver.zip');
+            const outStream = fs.createWriteStream('chormedriver');
+            const unZip = zlib.createDeflateRaw();
             inStream.pipe(unZip).pipe(outStream);
 
         }
@@ -59,8 +60,3 @@ const downLoadDriver = async () => {
 };
 
 downLoadDriver();
-
-
-
-
-
